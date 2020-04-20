7004fb1b39213ac79685bc70091d4a0926353100
B. Kelly <bk@ancilla.ca>
2020-04-20 08:54:43 -0400

    Add cljs support

--- linter.py
+++ linter.py
@@ -6,10 +6,17 @@ class ClojureKondoLinter(Linter):
   regex = r'^(?P<path>\S+):(?P<line>\d+):(?P<col>\d+): (?:(?P<warning>warning)|(?P<error>error)): (?P<message>.*)'
   multiline = False
   defaults = {
-    # TODO: support clojurescript as well.
-    # It has scope 'source.clojurescript', but we also need to replace
-    # --lang clj with --lang cljs
     'selector': 'source.clojure',
   }
   cmd = ['clj-kondo', '--lang', 'clj', '--lint', '-']
+  word_re = r'^([^][)(\s]+)'
+
+class ClojureScriptKondoLinter(Linter):
+  name = "cljs-kondo"
+  regex = r'^(?P<path>\S+):(?P<line>\d+):(?P<col>\d+): (?:(?P<warning>warning)|(?P<error>error)): (?P<message>.*)'
+  multiline = False
+  defaults = {
+    'selector': 'source.clojurescript',
+  }
+  cmd = ['clj-kondo', '--lang', 'cljs', '--lint', '-']
   word_re = r'^([^][)(\s]+)'
